4e62a30d816cc529f07b27a30aae2877fd7af194
devcanvas <0341531@mail.ru>
2024-04-13 15:45:16 +0300

    - сделала конструктор бота явным - через констркутор добавила команды под кнопку бота "Меню": /start, /stop, /random.

--- src/main/java/ru/wowdemobot/bot/service/TelegramBot.java
+++ src/main/java/ru/wowdemobot/bot/service/TelegramBot.java
@@ -1,22 +1,43 @@
 package ru.wowdemobot.bot.service;
 
 import lombok.RequiredArgsConstructor;
+import java.util.List;
+import java.util.ArrayList;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScope;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.wowdemobot.bot.config.properties.BotProperties;
 
 @Slf4j
 @Component
-@RequiredArgsConstructor
 public class TelegramBot extends TelegramLongPollingBot {
+
+
 
     private final BotProperties botProperties;
     private final MessageService messageService;
 
+    public TelegramBot(BotProperties botProperties, MessageService messageService) {
+        this.botProperties = botProperties;
+        this.messageService = messageService;
+
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("/start", "Начни"));
+        listOfCommands.add(new BotCommand("/stop", "Пока!"));
+        listOfCommands.add(new BotCommand("/random", "Дай совет по Java!"));
+        try {
+            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            log.error(e.getMessage());
+        }
+    }
     @Override
     public String getBotUsername() {
         return botProperties.name();
